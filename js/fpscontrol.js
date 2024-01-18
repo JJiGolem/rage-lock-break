43c4ab20951361d33374a649443d2a913b614063
JJiGolem <lorsanov2000@gmail.com>
2024-01-18 23:44:55 +0300

    Cleanup fpscontrol

--- js/fpscontrol.js
+++ js/fpscontrol.js
@@ -1,51 +1,51 @@
 class FpsControl {
-  constructor(fps, callback) {
-    this.fps = fps;
-    this.callback = callback;
+	constructor(fps, callback) {
+		this.fps = fps;
+		this.callback = callback;
 
-    this.isPlaying = false;
-    this.delay = 1000 / fps,
-		this.time = null,
-		this.frame = -1,
+		this.isPlaying = false;
+		this.delay = 1000 / fps;
+		this.time = null;
+		this.frame = -1;
 		this.tref;
 
-    this.loopBind = this.loop.bind(this);
-  }
+		this.loopBind = this.loop.bind(this);
+	}
 
 	loop(timestamp) {
-    if (!this.isPlaying) {
-      return;
-    }
+		if (!this.isPlaying) {
+			return;
+		}
 
 		if (this.time === null) {
-      this.time = timestamp;
-    }
+			this.time = timestamp;
+		}
 
 		const seg = Math.floor((timestamp - this.time) / this.delay);
 		if (seg > this.frame) {
 			this.frame = seg;
-      this.callback({
-        time: timestamp,
-        frame: this.frame
-      })
+			this.callback({
+				time: timestamp,
+				frame: this.frame
+			})
 		}
 		this.tref = requestAnimationFrame(this.loopBind)
 	}
-	
+
 	frameRate(newfps) {
 		this.fps = newfps;
 		this.delay = 1000 / this.fps;
 		this.frame = -1;
 		this.time = null;
 	};
-	
+
 	start() {
 		if (!this.isPlaying) {
 			this.isPlaying = true;
 			this.tref = requestAnimationFrame(this.loopBind);
 		}
 	};
-	
+
 	pause() {
 		if (this.isPlaying) {
 			cancelAnimationFrame(this.tref);
